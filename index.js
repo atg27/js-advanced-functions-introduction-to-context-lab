f1621ded2361dc70c56133606db14f800f37112d
Andrew <atg27@uw.edu>
2021-04-04 20:35:10 -0700

    commit tree 4b4d9dc50f53b3df8e7f09787b717133219fb842

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Your code here
+function createEmployeeRecord (employee){
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents:[],
+    }
+}
+
+function createEmployeeRecords(employees){
+    return employees.map((employee) => {
+        return createEmployeeRecord(employee);
+    })
+}
+
+function createTimeInEvent(employee, punchIn){
+    let timeInEvents = {
+        type: "TimeIn",
+        hour: parseInt(punchIn.split(' ')[1]),
+        date: punchIn.split(' ')[0],
+    }
+    employee.timeInEvents.push(timeInEvents)
+    return employee;
+}
+
+function createTimeOutEvent(employee, punchOut){
+    let timeOutEvents = {
+        type: "TimeOut",
+        hour: parseInt(punchOut.split(' ')[1]),
+        date: punchOut.split(' ')[0],
+    }
+    employee.timeOutEvents.push(timeOutEvents)
+    return employee;
+}
+
+function hoursWorkedOnDate(employee, date){
+    let timeIn = employee.timeInEvents.find(day => day.date ===date).hour;
+    let timeOut = employee.timeOutEvents.find(day => day.date ===date).hour;
+    return (timeOut -timeIn) / 100;
+}
+
+function wagesEarnedOnDate (employee, date){
+    return hoursWorkedOnDate(employee, date) * employee.payPerHour;
+}
+
+function allWagesFor(employees){
+
+}
